bc1990a51f8aa44bd05d3563f83a6bbccda35aed
RobinKopperud <Robin.kopperud@gmail.com>
2024-05-19 01:31:24 +0200

    s

--- Gaming/gamingui.php
+++ Gaming/gamingui.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gaming Hub</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/gaming.css">
 </head>
 <body>
     <div class="sidebar">

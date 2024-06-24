a7a22a894209607b020ff4412b1a66b07ae36579
RobinKopperud <Robin.kopperud@gmail.com>
2024-06-25 01:21:52 +0200

    s

--- MC/ref/timeline.php
+++ MC/ref/timeline.php
@@ -20,7 +20,7 @@ if ($result->num_rows > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tidslinje - Mental Racing Team</title>
-    <link rel="stylesheet" href="timeline.css?v=1.0">
+    <link rel="stylesheet" href="timeline.css?v=1.1">
 </head>
 <body>
     <header>

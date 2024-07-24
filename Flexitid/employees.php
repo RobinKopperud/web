fb2dcd0a3d67384b6e635c20d9806553a207acbb
RobinKopperud <Robin.kopperud@gmail.com>
2024-07-24 19:41:50 +0200

    ss

--- Flexitid/employees.php
+++ Flexitid/employees.php
@@ -29,6 +29,8 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fleksitid Ansatteside</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="employee.css">
+
 </head>
 <body>
     <nav class="navbar">

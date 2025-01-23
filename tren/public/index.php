583855f5a9413e75a3e2d9b87abce1477998ca0a
RobinKopperud <Robin.kopperud@gmail.com>
2025-01-23 22:55:13 +0100

    ss

--- tren/public/index.php
+++ tren/public/index.php
@@ -1,3 +1,13 @@
+<?php
+session_start(); // Start session to check if the user is logged in
+
+// Redirect to dashboard if the user is already logged in
+if (isset($_SESSION['user_id'])) {
+    header('Location: dashboard.php');
+    exit();
+}
+?>
+
 <?php include_once '../includes/header.php'; ?>
 
 <main>
